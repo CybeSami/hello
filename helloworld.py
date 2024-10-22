50756e16023f5e53e27faf208b3c3fc5fa17a727
CybeSami <sami.milhi@gmail.com>
2024-10-22 11:18:35 +0200

    Update helloworld02.py

--- helloworld.py
+++ helloworld.py
@@ -3,7 +3,7 @@ from tqdm import tqdm
 
 def hello_animation():
     message = "++++"
-    for char in tqdm(message, desc="Chargement :"):
+    for char in tqdm(message, desc="Chargement "):
         print(char, end='', flush=True)
         time.sleep(0.1)
     print("\nHello World !")
