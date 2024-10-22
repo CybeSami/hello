6184f0216e9e8887462ce372ef2329c7eaaa094c
CybeSami <sami.milhi@gmail.com>
2024-10-22 09:35:27 +0200

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -6,7 +6,7 @@ def hello_animation():
     for char in tqdm(message, desc="Affichage du message"):
         print(char, end='', flush=True)
         time.sleep(0.1)
-    print("\nMessage affiché avec succès!")
+    print("\nHello World !")
 
 if __name__ == "__main__":
     hello_animation()
